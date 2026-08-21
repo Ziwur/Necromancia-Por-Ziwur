f2ee66c79a7a8af8d8e2292b19d69b62024446cd
Ziwur <ziuamino@gmail.com>
2026-08-20 20:16:59 -0400

    Add files via upload

---
+++ necromancer-template-1.21.1/src/client/java/com/ziwur/necromancer/client/NecromancerClient.java
@@ -0,0 +1,10 @@
+package com.ziwur.necromancer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NecromancerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
